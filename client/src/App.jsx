4947cbec4dee24034e76f366e39903555bd0654d
tolga571 <fsm1543857@gmail.com>
2024-06-10 17:25:01 +0300

    complete create a post page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,40 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Projects from './pages/Projects';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import PrivateRoute from './components/PrivateRoute';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/sign-in' element={<SignIn />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route element={<PrivateRoute />} >
-            <Route path='/dasoard' element={<Dashboard />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/dasboard" element={<Dashboard />} />
           </Route>
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/projects' element={<Projects />} />
+          <Route element={<OnlyAdminPrivateRoute />}>
+            <Route path="/create-post" element={<CreatePost />} />
+          </Route>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
         <Footer />
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -21,9 +21,10 @@ import {
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 export default function DashProfile() {
-    const { currentUser, error } = useSelector((state) => state.user);
+    const { currentUser, error, loading } = useSelector((state) => state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -133,7 +134,7 @@ export default function DashProfile() {
         try {
             dispatch(deleteUserStart());
             const res = await fetch(`/api/user/delete/${currentUser._id}`, {
-                method: 'DELETE',
+                method: "DELETE",
             });
             const data = await res.json();
             if (!res.ok) {
@@ -148,8 +149,8 @@ export default function DashProfile() {
 
     const handleSignout = async () => {
         try {
-            const res = await fetch('/api/user/signout', {
-                method: 'POST',
+            const res = await fetch("/api/user/signout", {
+                method: "POST",
             });
             const data = await res.json();
             if (!res.ok) {
@@ -160,7 +161,7 @@ export default function DashProfile() {
         } catch (error) {
             console.log(error.message);
         }
-    }
+    };
     return (
         <div className="max-w-lg mx-auto p-3 w-full">
             <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
@@ -228,15 +229,33 @@ export default function DashProfile() {
                     placeholder="password"
                     onChange={handleChange}
                 />
-                <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-                    Update
+                <Button
+                    type="submit"
+                    gradientDuoTone="purpleToBlue"
+                    outline
+                    disabled={loading || imageFileUploading}
+                >
+                    {loading ? 'Loading...' : 'Update'}
                 </Button>
+                {currentUser.isAdmin && (
+                    <Link to={"/create-post"}>
+                        <Button
+                            type="button"
+                            gradientDuoTone="purpleToPink"
+                            className="w-full"
+                        >
+                            Create a post
+                        </Button>
+                    </Link>
+                )}
             </form>
             <div className="text-red-500 flex justify-between mt-5">
                 <span onClick={() => setShowModal(true)} className="cursor-pointer">
                     Delete Account
                 </span>
-                <span onClick={handleSignout} className="cursor-pointer">Sign Out</span>
+                <span onClick={handleSignout} className="cursor-pointer">
+                    Sign Out
+                </span>
             </div>
             {updateUserSuccess && (
                 <Alert color="success" className="mt-5">
